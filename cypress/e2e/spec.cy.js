46777230b674c513094c7abcb31d9576d2aa50a0
SIJED <Danilcarol56@gmail.com>
2024-03-18 17:20:30 +0700

    authorization

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,38 +1,98 @@
-describe('Cypress Tests', () => {
+describe('None-existent authorization', () => {
+  it('None-existent password test', () =>{
+    cy.fixture('cypressTest').then(data => {
+      cy.log('Переход на страницу авторизации')
+      cy.visit(data.main_url + 'login/')
+
+      cy.log('Ввод существующего логина')
+      cy.get('input[class="form-input--text form-input"]')
+          .type(data.login)
+
+      cy.log('Ввод существующего пароля')
+      cy.get('input[class="form-input--password form-input"]')
+          .type(data.none_existent_password)
+
+      cy.log('Клик по кнопке "Войти"')
+      cy.get('.button__background-color-green')
+          .click()
+
+      cy.wait(3000)
+
+      cy.log('Проверка что появился элемент сигнализирующий об ошибке')
+      cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+    })
+  })
+})
+
+describe('None-existent authorization', () => {
   it('None-existent login test', () =>{
     cy.fixture('cypressTest').then(data => {
       cy.log('Переход на страницу авторизации')
-      cy.visit(data.main_url )
+      cy.visit(data.main_url + 'login/')
 
       cy.log('Ввод несуществующего логина')
-      cy.get('input[id="passp-field-login"]')
+      cy.get('input[class="form-input--text form-input"]')
           .type(data.none_existent_login)
 
+      cy.log('Ввод существующего пароля')
+      cy.get('input[class="form-input--password form-input"]')
+          .type(data.password)
+
       cy.log('Клик по кнопке "Войти"')
-      cy.get('div[class="passp-button passp-sign-in-button"] button[type="submit"]')
+      cy.get('.button__background-color-green').click()
+
+      cy.wait(3000)
+
+      cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+    })
+  })
+
+})
+
+describe('existent authorization', () => {
+  it('existent test', () =>{
+    cy.fixture('cypressTest').then(data => {
+      cy.log('Переход на страницу авторизации')
+      cy.visit(data.main_url + 'login/')
+
+      cy.log('Ввод существующего логина')
+      cy.get('input[class="form-input--text form-input"]')
+          .type(data.login)
+
+      cy.log('Ввод существующего пароля')
+      cy.get('input[class="form-input--password form-input"]')
+          .type(data.password)
+
+      cy.log('Клик по кнопке "Войти"')
+      cy.get('.button__background-color-green').click()
+
+      cy.wait(3000)
+    })
+  })
+})
+
+describe('None-existent registration', () => {
+  it('None-existent login test', () =>{
+    cy.fixture('registration').then(data => {
+      cy.log('Переход на страницу регистрации')
+      cy.visit(data.main_url + 'registration/')
+
+      cy.log('Ввод не подходящего логина')
+      cy.get('input[class="form-input--text form-input"]')
+          .type(data.login)
+
+      cy.log('Ввод существующего пароля')
+      cy.get('input[class="form-input--password form-input"]')
+          .type(data.none_existent_password)
+
+      cy.log('Клик по кнопке "Войти"')
+      cy.get('.button__background-color-green')
           .click()
 
+      cy.wait(3000)
+
       cy.log('Проверка что появился элемент сигнализирующий об ошибке')
-      cy.get('div[class="passp-form-field_error"]').should('exist')
+      cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
     })
-  }),
-
-      it('None-existent password test', () =>{
-        cy.fixture('cypressTest').then(data => {
-          cy.log('Переход на страницу авторизации')
-          cy.visit(data.main_url + 'login/')
-
-
-
-          cy.log('Ввод несуществующего пароля')
-          cy.get('input[id = "passp-field-passwd"]')
-              .type(data.none_existent_password)
-
-          cy.log('Клик по кнопке "Войти"')
-          cy.get('div[class="passp-button passp-sign-in-button"] button[type="submit"]')
-              .click()
-
-
-        })
-      })
-});
+  })
+})
