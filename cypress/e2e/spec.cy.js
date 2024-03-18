01a9a120bf47e9ce2a359e582f8c5e42bcefa636
SIJED <Danilcarol56@gmail.com>
2024-03-18 19:06:17 +0700

    registration

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-describe('None-existent authorization', () => {
+describe('None-existent authorization login', () => {
   it('None-existent password test', () =>{
     cy.fixture('cypressTest').then(data => {
       cy.log('Переход на страницу авторизации')
@@ -8,7 +8,7 @@ describe('None-existent authorization', () => {
       cy.get('input[class="form-input--text form-input"]')
           .type(data.login)
 
-      cy.log('Ввод существующего пароля')
+      cy.log('Ввод несуществующего пароля')
       cy.get('input[class="form-input--password form-input"]')
           .type(data.none_existent_password)
 
@@ -24,7 +24,7 @@ describe('None-existent authorization', () => {
   })
 })
 
-describe('None-existent authorization', () => {
+describe('None-existent authorization password', () => {
   it('None-existent login test', () =>{
     cy.fixture('cypressTest').then(data => {
       cy.log('Переход на страницу авторизации')
@@ -71,28 +71,94 @@ describe('existent authorization', () => {
   })
 })
 
-describe('None-existent registration', () => {
+describe('None-existent registration login', () => {
   it('None-existent login test', () =>{
     cy.fixture('registration').then(data => {
       cy.log('Переход на страницу регистрации')
       cy.visit(data.main_url + 'registration/')
 
       cy.log('Ввод не подходящего логина')
-      cy.get('input[class="form-input--text form-input"]')
-          .type(data.login)
+      cy.get('input[class="form-input--text form-input"]').eq(0)
+          .type(data.none_existent_login_register)
 
-      cy.log('Ввод существующего пароля')
-      cy.get('input[class="form-input--password form-input"]')
-          .type(data.none_existent_password)
-
-      cy.log('Клик по кнопке "Войти"')
-      cy.get('.button__background-color-green')
-          .click()
-
-      cy.wait(3000)
 
       cy.log('Проверка что появился элемент сигнализирующий об ошибке')
       cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+
+      cy.wait(3000)
+    })
+  })
+})
+
+describe('None-existent registration email', () => {
+  it('None-existent email test', () =>{
+    cy.fixture('registration').then(data => {
+      cy.log('Переход на страницу регистрации')
+      cy.visit(data.main_url + 'registration/')
+
+
+      cy.log('Ввод не походящей почты')
+      cy.get('input[class="form-input--email form-input"]').eq(0)
+          .type(data.none_existent_email_register)
+
+      cy.log('Проверка что появился элемент сигнализирующий об ошибке')
+      cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+
+      cy.wait(3000)
+    })
+  })
+})
+
+describe('None-existent registration password', () => {
+  it('None-existent password test', () =>{
+    cy.fixture('registration').then(data => {
+      cy.log('Переход на страницу регистрации')
+      cy.visit(data.main_url + 'registration/')
+
+      cy.log('Ввод не походящего пароля')
+      cy.get('input[class="form-input--password form-input"]').eq(0)
+          .type(data.none_existent_password_register)
+
+      cy.log('Ввод не походящего пароля')
+      cy.get('input[class="form-input--password form-input"]').eq(1)
+          .type(data.none_existent_password_register)
+
+
+      cy.log('Проверка что появился элемент сигнализирующий об ошибке')
+      cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+
+      cy.wait(3000)
+    })
+  })
+})
+
+describe('existent registration', () => {
+  it('None-existent email test', () =>{
+    cy.fixture('registration').then(data => {
+      cy.log('Переход на страницу регистрации')
+      cy.visit(data.main_url + 'registration/')
+
+      cy.log('Ввод подходящего логина')
+      cy.get('input[class="form-input--text form-input"]').eq(0)
+          .type(data.login_register)
+
+      cy.log('Ввод походящей почты')
+      cy.get('input[class="form-input--email form-input"]').eq(0)
+          .type(data.email_register)
+
+      cy.log('Ввод походящего пароля')
+      cy.get('input[class="form-input--password form-input"]').eq(0)
+          .type(data.password_register)
+
+      cy.log('Ввод походящего пароля')
+      cy.get('input[class="form-input--password form-input"]').eq(1)
+          .type(data.password_register)
+
+
+      cy.log('Клик по кнопке "Войти"')
+      cy.get('.button__background-color-green').eq(1)
+          .click()
+
     })
   })
 })
